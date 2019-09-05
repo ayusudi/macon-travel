8307b4f8f9a87be1ad93041c9a2e6fab997c4ded
TatagW <tatagwibowo95@gmail.com>
2019-09-05 19:08:42 +0700

    skeleton

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router()
-const mapsRouter = require('./gmaps')
 router.get('/',(req, res, next)=>{
     console.log('connected')
 })
-router.use('/maps', mapsRouter)
 
 module.exports = router
